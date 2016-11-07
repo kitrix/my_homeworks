bb7f3af147e4873418e16316490f0879aef89036
Ekaterina Afanaseva <kitrix97@gmail.com>
2016-11-07 20:14:50 +0300

    Delete hw2.py

--- Python_hw_2/hw2.py
+++
@@ -1,8 +0,0 @@
-word = input ('Введите слово: ')
-indx = len(word)-1
-while indx >= 0:
-    if (word[indx]!= 'я') & (word[indx]!= 'з') :
-            print (word[indx])
-            indx -= 1
-    else:
-        indx -= 1
